f71d5091ac48bfbcb5b5e5876405a4395404c726
ParkEunbin <44806572+ParkEunbin@users.noreply.github.com>
2018-11-18 19:11:32 +0900

    개인정보

--- Chapter5/5-5.py
+++ Chapter5/5-5.py
@@ -1,8 +1,9 @@
+#개인정보
 person={}
-person["이름"]='박은빈'
-person["성별"]='여'
+person["이름"]='홍길동'
+person["성별"]='남'
 person["나이"]=20
-person["키"]=160
+person["키"]=180
 
 for key in person:
     print("%s:%s" %(key,person[key]))
